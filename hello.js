32274ba2edf661d78e6dcb1b3020ea7601bd48de
nikhil ahirwar <nikhilraj2908@gmail.com>
2024-06-07 21:13:58 +0530

    add: netflix page

---
+++ hello.js
@@ -0,0 +1,94 @@
+// // let a=10;
+// // var b=20;
+// // console.log(a+b);
+// // for(let i=1;i<5;i++){
+// //     console.log("hello");
+// // }
+// var a=12
+// var b="12"
+// console.log(a+b)
+// console.log(typeof(a+b))
+//  c={
+//     name1:"nihil",
+//     lastname:"ahirwar",
+//     age:22
+
+// }
+// c.name1="ram"
+// console.log(c)
+// let a=15
+// if (a>10 && a<20){
+//     console.log("age is valid");
+// }
+// else{
+//     console.log("not valid")
+// }
+// switch (true) {
+//     case (a<10):
+//         console.log("invalid")
+//         break
+//     case (a<=20):
+//         console.log("valid")
+//         break
+//     default:
+//         console.log("not in range")
+// }
+// a=19
+// console.log((a>18)?"valid" :"invalid")
+// Name="nikhil"
+// for (var i in Name){
+//     console.log(Name[i])
+// }
+
+// a="rammHhaN"
+// console.log(a.toUpperCase())
+// console.log(typeof(a.toLowerCase()))
+// console.log(a)
+// console.log(a.slice(1))
+// a=[12,22,'nikk',44,32,true]
+// for(let i=0;i<a.length;i++){
+//     console.log(a[i])
+// }
+// let arr=[11,22,33,44,33]
+// let b=arr.map((a)=>{
+//     return a=a+1
+// })
+// console.log(arr)
+// console.log(b)
+
+// let c=arr.filter((a)=>{
+//     return a<15
+// })
+// console.log(c)
+// let arr=[1,2,3,4,11,22,33,10,20]
+// let b=arr.filter((a)=>{
+//     return a%10==0
+// })
+// console.log(b)
+// let arr=[1,2,3,4,11,22,33,10,20]
+// let b=arr.map((a)=>{
+//     return a*a
+// })
+// console.log(b)
+
+// var a=math.random()
+// do{
+//     b=prompt("enter your guess")
+//     if (b<a){
+//         console.log("guess is too low")
+//     }
+//     else if(b>a){
+//         console.log("guess is too high")
+//     }
+//     else if(b==0 || b<0){
+//         console.log("u quit")
+//         break
+//     }
+//     else {
+//         console.log("u guess the number")
+//     }
+// }while(a==b)
+// a={1:"nikilkm",2:"ram", 3:"shyam"}
+// console.table(a)
+// console.warn("dont do")
+// console.table(console)
